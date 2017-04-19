ce057dd2e8d8749d33622dd12a4a2b739cff178c
Unknown <unknown@example.com>
2017-04-19 14:28:37 +0300

    re

--- creation.sql
+++ creation.sql
@@ -3,26 +3,22 @@
 --drop table relationship; drop table stories; drop table results; drop table jokes; drop table people;
 
 CREATE TABLE people(
-<<<<<<< Updated upstream
-	id			int				PRIMARY KEY,
-=======
-	id		int	PRIMARY KEY,
->>>>>>> Stashed changes
+	id		int		PRIMARY KEY,
 	name 		varchar(100) 	NOT NULL
 );
 
 CREATE TABLE jokes(
-	id 			int 			PRIMARY KEY,
-	title 		varchar(100) 	DEFAULT '',
+	id 		int 			PRIMARY KEY,
+	title 		varchar(100)		DEFAULT '',
 	date 		date,
 	from_who 	int 			REFERENCES people(id),
 	to_who 		int 			REFERENCES people(id)
 );
 	
 CREATE TABLE relationship(
-	person1 	int				REFERENCES people(id),
+	person1 	int			REFERENCES people(id),
 	person2		int 			REFERENCES people(id),
-	rel 		varchar(100)	NOT NULL
+	rel 		varchar(100)		NOT NULL
 );
 	
 CREATE TABLE stories(
